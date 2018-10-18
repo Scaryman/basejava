5c0259fba773a3e981c357a18ae02eeedf26bf4c
scaryman <scaryman86@gmail.com>
2018-10-18 23:17:04 +0300

    Перенес все в absract class

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -2,11 +2,42 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage{
 
     protected static final int STORAGE_LIMIT = 100000;
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
+
+    @Override
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            storage[index] = r;
+        } else {
+            System.out.println("Резюме не найдено");
+        }
+    }
+
+    @Override
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            if (index + 1 < size) {
+                System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+            }
+            size--;
+            storage[size] = null;
+        } else {
+            System.out.println("Резюме не найдено");
+        }
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
 
     public int size() {
         return size;
@@ -19,6 +50,14 @@ public abstract class AbstractArrayStorage implements Storage{
             return null;
         }
         return storage[index];
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    @Override
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
     }
 
     protected abstract int getIndex(String uuid);
--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -2,18 +2,10 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage{
-
-    @Override
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
 
     @Override
     public void save(Resume r) {
@@ -25,38 +17,6 @@ public class ArrayStorage extends AbstractArrayStorage{
             storage[size] = r;
             size++;
         }
-    }
-
-    @Override
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index >= 0) {
-            storage[index] = r;
-        } else {
-            System.out.println("Резюме не найдено");
-        }
-    }
-
-    @Override
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index >= 0) {
-            if (index + 1 < size) {
-                System.arraycopy(storage, index + 1, storage, index, size - index - 1);
-            }
-            size--;
-            storage[size] = null;
-        } else {
-            System.out.println("Резюме не найдено");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    @Override
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
     }
 
     @Override
