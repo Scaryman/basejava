6c17a734cb02cd4dddccd1ced7c621365897be39
scaryman <scaryman86@gmail.com>
2018-12-10 06:48:35 +0300

    Lesson 07 - Singleton - Enum

---
+++ src/TestSingleton.java
@@ -0,0 +1,29 @@
+import ru.javawebinar.basejava.model.SectionType;
+
+public class TestSingleton {
+    private static TestSingleton instance;
+
+    public static TestSingleton getInstance() {
+        if (instance == null) {
+            instance = new TestSingleton();
+        }
+        return instance;
+    }
+
+    private TestSingleton() {
+    }
+
+    public enum Singleton {
+        INSTANCE
+    }
+
+    public static void main(String[] args) {
+        TestSingleton.getInstance().toString();
+        Singleton instance = Singleton.valueOf("INSTANCE");
+        System.out.println(instance.ordinal());
+
+        for (SectionType type : SectionType.values()) {
+            System.out.println(type.getTitle());
+        }
+    }
+}
